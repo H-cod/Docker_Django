688da78deaa088b05f4aaf0e4512924e467682bf
Hayk Nersesyan <hayknersesyanaj@gmail.com>
2022-04-17 22:37:00 +0300

    Manage user

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserCreateAPIView, CreateTokenView
+from .views import UserCreateAPIView, CreateTokenView, ManageUserView
 
 app_name = 'user'
 
 urlpatterns = [
     path('create/', UserCreateAPIView.as_view(), name='create'),
     path('token/', CreateTokenView.as_view(), name='token'),
+    path('me/', ManageUserView.as_view(), name='me'),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,10 +1,11 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework import generics, authentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework import permissions
 from .serializers import UserCreateSerializer, AuthTokenSerializer
 
 
-class UserCreateAPIView(CreateAPIView):
+class UserCreateAPIView(generics.CreateAPIView):
     """Create new user in the system"""
     serializer_class = UserCreateSerializer
 
@@ -13,3 +14,14 @@ class CreateTokenView(ObtainAuthToken):
     """Create new auth token for user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """ Manage the authenticated user """
+    serializer_class = UserCreateSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return authenticated user"""
+        return self.request.user
